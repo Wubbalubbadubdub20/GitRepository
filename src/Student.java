4c3139e521ba8a638e46c36c1c613bf07704fa18
HuXuanHao <14165601+HuXuanHao@user.noreply.gitee.com>
2024-05-23 18:04:14 +0800

    第二次提交数据，增加Student类！

---
+++ src/Student.java
@@ -0,0 +1,22 @@
+public class Student {
+    public Student() {
+    }
+    int age;
+    int id;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
